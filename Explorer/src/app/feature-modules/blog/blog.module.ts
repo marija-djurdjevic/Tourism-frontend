b99cbb623576b9f4b49396b5a490e7cecddd98cf
Ivan Radmilovic <radmilovicivan@gmail.com>
2024-10-16 12:37:35 +0200

    feat: added comment button on each blog

--- Explorer/src/app/feature-modules/blog/blog.module.ts
+++ Explorer/src/app/feature-modules/blog/blog.module.ts
@@ -4,21 +4,24 @@ import { CommentComponent } from './comment/comment.component';
 import { MaterialModule } from 'src/app/infrastructure/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommentFormComponent } from './comment-form/comment-form.component';
+import { BlogOverview } from './blog-overview/blog-overview-component';
 
 
 
 @NgModule({
   declarations: [
     CommentComponent,
-    CommentFormComponent
+    CommentFormComponent,
+    BlogOverview
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   exports: [
-    CommentComponent
+    CommentComponent,
+    BlogOverview
   ]
 })
 export class BlogModule { }
