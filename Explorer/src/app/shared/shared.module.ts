1ec121b4beb5f274a432ca4489ecfa870e180781
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2024-10-20 16:40:53 +0200

    feat: add basic map display

--- Explorer/src/app/shared/shared.module.ts
+++ Explorer/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageComponent } from './image/image.component';
+import { MapComponent } from './map/map.component';
 
 
 
 @NgModule({
   declarations: [
-    ImageComponent
+    ImageComponent,
+    MapComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ImageComponent
+    ImageComponent, MapComponent
   ],
+  
 })
 export class SharedModule { }
