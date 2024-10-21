c983c3526a21c851f66efef8960c0e7c8e4f17a1
pili01 <pilipovicdusko002@gmail.com>
2024-10-21 21:14:18 +0200

    Enabled map usage for selecting a location when creating a key point.

--- Explorer/src/app/shared/map/map.component.ts
+++ Explorer/src/app/shared/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { MapService } from './map.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -11,13 +12,15 @@ export class MapComponent implements AfterViewInit {
   @Input() keyPoints: any[] = [];
   @Input() initialCenter: [number, number] = [45.2396, 19.8227];
   @Input() initialZoom: number = 13;
-  @Input() markers: any[] = []; 
-  @Output() keyPointSelected = new EventEmitter<{ latitude: number, longitude: number }>(); 
-  @Output() markerAdded = new EventEmitter<{ latitude: number, longitude: number }>(); 
+  @Input() markers: any[] = [];
+  @Input() creatingKeyPoint = false;
+  @Output() keyPointSelected = new EventEmitter<{ latitude: number, longitude: number }>();
+  @Output() markerAdded = new EventEmitter<{ latitude: number, longitude: number }>();
   private map: any;
-  searchQuery: string = ''; 
+  searchQuery: string = '';
+  address:string='';
 
-  constructor(private service: MapService) {}
+  constructor(private service: MapService) { }
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -45,17 +48,17 @@ export class MapComponent implements AfterViewInit {
 
   setRoute(waypoints: Array<{ lat: number, lng: number }>, profile: 'walking' | 'driving' | 'cycling'): void {
     const routeControl = L.Routing.control({
-      waypoints: waypoints.map(point => L.latLng(point.lat, point.lng)),  
-      router: L.routing.mapbox('pk.eyJ1IjoiZGp1cmRqZXZpY20iLCJhIjoiY20yaHVzOTgyMGJwbzJqczNteW1xMm0yayJ9.woKtBh92sOV__L25KcUu_Q', { profile: `mapbox/${profile}` }) 
+      waypoints: waypoints.map(point => L.latLng(point.lat, point.lng)),
+      router: L.routing.mapbox('pk.eyJ1IjoiZGp1cmRqZXZpY20iLCJhIjoiY20yaHVzOTgyMGJwbzJqczNteW1xMm0yayJ9.woKtBh92sOV__L25KcUu_Q', { profile: `mapbox/${profile}` })
     }).addTo(this.map);
-  
-    routeControl.on('routesfound', function(e) {
+
+    routeControl.on('routesfound', function (e) {
       const routes = e.routes;
       const summary = routes[0].summary;
       alert('Total distance is ' + (summary.totalDistance / 1000).toFixed(2) + ' km and total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
     });
   }
-  
+
 
   ngAfterViewInit(): void {
     let DefaultIcon = L.icon({
@@ -64,6 +67,36 @@ export class MapComponent implements AfterViewInit {
 
     L.Marker.prototype.options.icon = DefaultIcon;
     this.initMap();
+    if (this.creatingKeyPoint) {
+      this.setCurrentLocation();
+    }
+  }
+
+  private setCurrentLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        // Postavljanje trenutne lokacije kao centar mape
+        this.map.setView([latitude, longitude], this.initialZoom);
+
+        // Dodavanje markera na trenutnu lokaciju
+        const mp = new L.Marker([latitude, longitude]).addTo(this.map)
+          .bindPopup('You are here!')
+          .openPopup();
+        this.markers.push(mp)
+        this.keyPointSelected.emit({ latitude: latitude, longitude: longitude });
+        this.service.reverseSearch(latitude, longitude).subscribe((response) => {
+          this.address = response.display_name; // Dobijena adresa
+          console.log('Dobijena adresa:', this.address);
+        });
+        // Emitovanje trenutnih koordinata
+        this.markerAdded.emit({ latitude, longitude });
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
   }
 
   search(): void {
@@ -72,11 +105,21 @@ export class MapComponent implements AfterViewInit {
         if (result.length > 0) {
           const lat = result[0].lat;
           const lon = result[0].lon;
-          this.map.setView([lat, lon], 15); 
-          L.marker([lat, lon])
+
+          if (this.creatingKeyPoint) {
+            this.clearMarkers();
+          }
+          this.keyPointSelected.emit({ latitude: lat, longitude: lon });
+          this.map.setView([lat, lon], 15);
+          const mp = new L.Marker([lat, lon])
             .addTo(this.map)
             .bindPopup(this.searchQuery)
             .openPopup();
+          this.markers.push(mp)
+          this.service.reverseSearch(lat, lon).subscribe((response) => {
+            this.address = response.display_name; // Dobijena adresa
+            console.log('Dobijena adresa:', this.address);
+          });
         } else {
           alert('Location not found.');
         }
@@ -86,20 +129,34 @@ export class MapComponent implements AfterViewInit {
       },
     });
   }
-    
+
   registerOnClick(): void {
     this.map.on('click', (e: any) => {
       const coord = e.latlng;
       const lat = coord.lat;
       const lng = coord.lng;
-      this.keyPointSelected.emit({ latitude: lat, longitude: lng }); 
+      if (this.creatingKeyPoint) {
+        this.clearMarkers();
+      }
+      this.keyPointSelected.emit({ latitude: lat, longitude: lng });
       this.service.reverseSearch(lat, lng).subscribe((res) => {
         console.log(res.display_name);
       });
       const mp = new L.Marker([lat, lng]).addTo(this.map);
+      this.markers.push(mp);
+      this.service.reverseSearch(lat, lng).subscribe((response) => {
+        this.address = response.display_name; // Dobijena adresa
+        console.log('Dobijena adresa:', this.address);
+      });
       this.markerAdded.emit({ latitude: lat, longitude: lng }); // Emit the added marker
     });
   }
 
+  clearMarkers(): void {
+    this.markers.forEach(marker => {
+      this.map.removeLayer(marker);
+    });
+    this.markers = [];
+  }
 
 }
