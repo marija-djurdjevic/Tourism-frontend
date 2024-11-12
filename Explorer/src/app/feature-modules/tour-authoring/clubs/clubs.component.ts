68367c3a51adffb34131a88835705ac5eea9df16
Đorđe Ristić <djordjer88@gmail.com>
2024-11-12 16:16:22 +0100

    ui: clubs frontend improvement

--- Explorer/src/app/feature-modules/tour-authoring/clubs/clubs.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/clubs/clubs.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Clubs } from '../model/clubs.model';
 import { TourAuthoringService } from '../tour-authoring.service';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { ImageService } from 'src/app/shared/image.service';
 
 @Component({
   selector: 'xp-clubs',
@@ -13,17 +14,35 @@ export class ClubsComponent implements OnInit {
   selectedClub: Clubs;
   shouldRenderClubForm: boolean = false;
   shouldEdit: boolean = false;
+  image: File;
 
-  constructor(private service: TourAuthoringService) {}
+  constructor(private service: TourAuthoringService, private imageService: ImageService, private cd: ChangeDetectorRef) {
+    imageService.setControllerPath("tourist/image");
+  }
 
   ngOnInit(): void {
       this.getClubs();
+
   }
   getClubs(): void {
     this.service.getClubs().subscribe({
       next: (result: PagedResults<Clubs>) => {
         console.log(result)
         this.clubs = result.results
+        // kod za ucitavanje slike po id
+        this.clubs.forEach(element => {
+          this.imageService.getImage(element.imageId.valueOf()).subscribe((blob: Blob) => {
+            console.log(blob);  // Proveri sadržaj Blob-a
+            if (blob.type.startsWith('image')) {
+              element.image = URL.createObjectURL(blob);
+              this.cd.detectChanges();
+            } else {
+              console.error("Blob nije slika:", blob);
+            }
+          });
+
+        });
+        //kraj
       },
   error: (err: any) => {
     console.log(err)
