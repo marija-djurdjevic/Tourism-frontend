b1a2e4525bff1f0ae4d47ab356e9786b09ff0709
Paola Mitrovic <paolamitrovic02@gmail.com>
2024-11-26 13:00:22 +0100

    ui: Modified form in encounter and search button in map

--- Explorer/src/app/shared/shared.module.ts
+++ Explorer/src/app/shared/shared.module.ts
@@ -4,6 +4,8 @@ import { ImageComponent } from './image/image.component';
 import { MapComponent } from './map/map.component';
 import { FormsModule } from '@angular/forms'; 
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
     ImageComponent, MapComponent
