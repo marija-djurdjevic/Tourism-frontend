627615faf40476bba8c8ee42a206813516890714
pili01 <pilipovicdusko002@gmail.com>
2025-01-23 00:44:10 +0100

    search for tours added

--- Explorer/src/app/feature-modules/marketplace/model/search-by-distance.model.ts
+++ Explorer/src/app/feature-modules/marketplace/model/search-by-distance.model.ts
@@ -1,5 +1,16 @@
 export interface SearchByDistance {
-    distance: number;
+    distance: number; // Dodano za maksimalnu udaljenost od odabrane lokacije
+    minDistance: number;
+    maxDistance: number;
     longitude: number; 
     latitude: number; 
+    tags: string;
+    keyPointName: string;
+    maxRating: number;
+    minRating: number;
+    maxPrice: number;
+    minPrice: number;
+    maxDuration: number;
+    minDuration: number;
+    name: string;
   }
